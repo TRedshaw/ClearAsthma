a309a9b41641ccab166bc807f8d699752e5de02b
TRedshaw <toby@redshaw.me>
2023-01-15 15:31:18 +0000

    Added missing import

--- Clear/views.py
+++ Clear/views.py
@@ -10,7 +10,7 @@ from django.views.generic import View
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.contrib.auth import authenticate,login,logout
 from django.shortcuts import redirect
 from django.contrib import messages
